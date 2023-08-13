85622aa900e31b619b29626dc5840dad7aa0ddd8
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-08-13 15:49:09 +0200

    Nested router to router

---
+++ pages/product/[productId]/review/[reviewId].js
@@ -0,0 +1,9 @@
+import { useRouter } from "next/router"
+
+const Review = () => {
+    const router = useRouter()
+    const { productId, reviewId } = router.query
+    return <h1>Review {reviewId} for product {productId}</h1>
+}
+
+export default Review
