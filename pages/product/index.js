4da1c72ece6485262c4b59e74cae9f04b828f487
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-08-13 15:23:56 +0200

    Add dynamic router by using useRouter

---
+++ pages/product/index.js
@@ -0,0 +1,11 @@
+const ProductList = () => {
+    return (
+        <>
+            <h2>Product 1</h2>
+            <h2>Product 2</h2>
+            <h2>Product 3</h2>
+        </>
+    )
+}
+
+export default ProductList
---
+++ pages/product/sweater.js
@@ -0,0 +1,5 @@
+const Sweater = () => {
+    return <h1>Landing page for Sweaters</h1>
+}
+
+export default Sweater
