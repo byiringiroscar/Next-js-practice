3e3e776d1c76622a60e59fd80b596d00cb8d2029
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-08-13 16:52:37 +0200

    Routing from page to another page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Link from "next/link"
+
 const Home = () => {
-    return <h1>Home Page</h1>
+    return (
+        <div>
+            <h1>Home Page</h1>
+            <Link href="/blog">
+                Blog
+            </Link>
+            <br />
+            <Link href="/product">Products</Link>
+        </div>
+    )
 }
 
 export default Home
--- pages/product/index.js
+++ pages/product/index.js
@@ -1,6 +1,9 @@
+import Link from "next/link"
+
 const ProductList = () => {
     return (
         <>
+        <Link href="/">Home</Link>
             <h2>Product 1</h2>
             <h2>Product 2</h2>
             <h2>Product 3</h2>
